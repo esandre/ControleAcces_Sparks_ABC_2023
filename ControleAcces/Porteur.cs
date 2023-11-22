75e2a78e19cc725e1b12543c06008191ea85fda1
Enzo Sandré <atelier@enzosandre.fr>
2023-11-22 10:06:29 +0100

    Example d'implémentation de Conforme<>

--- ControleAcces/Porteur.cs
+++ ControleAcces/Porteur.cs
@@ -2,4 +2,44 @@
 
 public class Porteur
 {
+    public string Désignation { get; }
+}
+
+public interface ISpécification<in TRéel>
+{
+    bool EstConforme(TRéel réel);
+}
+
+public class NomSansEspacesSpécification  : ISpécification<Porteur>
+{
+    /// <inheritdoc />
+    public bool EstConforme(Porteur réel)
+    {
+        return !réel.Désignation.Contains(' ');
+    }
+}
+
+public class Conforme<TRéel, TSpecification> 
+    where TSpecification : ISpécification<TRéel>
+{
+    private readonly TRéel _réel;
+    private readonly TSpecification _specification;
+
+    public Conforme(TRéel réel, TSpecification specification)
+    {
+        _réel = réel;
+        _specification = specification;
+    }
+
+    public TReturn MapIfConform<TReturn>(
+        Func<TRéel, TReturn> mappingFunction, 
+        TReturn ifNotConform)
+        => _specification.EstConforme(_réel) 
+            ? mappingFunction(_réel) 
+            : ifNotConform;
+}
+
+public void DoSomething(Conforme<Porteur, NomSansEspacesSpécification> porteur)
+{
+    var identifiantPorteur = porteur.MapIfConform(p => p.Désignation, "Invalid");
 }
